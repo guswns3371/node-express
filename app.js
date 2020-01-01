8c2cb929a8dba37dceefcc41c96a5d642c8fe58f
guswn <guswns3371@gmail.com>
2020-01-01 18:26:15 +0900

    aaa

--- app.js
+++ app.js
@@ -3,18 +3,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mustacheExpress = require('mustache-express');
+var exphbs  = require('express-handlebars');
 
+//라우터들
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var talkRouter = require('./routes/talk');
+var feedRouter = require('./routes/feed');
+var tvRouter = require('./routes/tv');
+var facetimeRouter = require('./routes/facetime');
+var commRouter = require('./routes/comm');
+var musicRouter = require('./routes/music').default;
+
 
 var app = express();
 
 // view engine setup
-app.engine('mustache',
-    mustacheExpress(__dirname + '/views/partials', '.mustache'));
-// app.set('view engine', 'pug'); //pug 말고 mustache 쓸꺼야!
-app.set('view engine', 'mustache');
+app.engine('.handlebars', exphbs({
+  extname : 'handlebars',
+  defaultLayout: 'main',
+  layoutsDir : __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+}));
+//https://hackersandslackers.com/handlebars-templates-expressjs/
+// app.set('view engine', 'pug'); //pug 말고 handlebars 쓸꺼야!
+app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(logger('dev'));
@@ -23,8 +36,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//라우팅
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/talk', talkRouter);
+app.use('/feed', feedRouter);
+app.use('/tv', tvRouter);
+app.use('/facetime', facetimeRouter);
+app.use('/comm', commRouter);
+app.use('/music', musicRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,6 +59,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
+  console.log(res.locals.message);
   res.render('error');
 });
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('main', {
+    title: 'Welcome to Cocoa'
+  });
 });
 
 module.exports = router;
